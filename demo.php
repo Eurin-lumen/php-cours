bad12140a458dc6b6b25764687e3c3ae32d8fbd9
eurin-lumen <eurindelagoldcoast@gmail.com>
2022-07-15 00:44:30 +0100

    Etude boucle : partie 1

--- demo.php
+++ demo.php
@@ -1,15 +1,16 @@
 <?php
-$notes = [10, 15, 16];
-$eleves = [
-    'cm2' => ['jean', 'eurin', 'merveille', 'victoire'],
-    'cm1' => ['eurin', 'belle', 'elsa', 'auri']
-];
-
-    foreach ($eleves as $classe => $listEleves) {
-        # code...
-        echo "la classe $classe : \n";
-        foreach($listEleves as $eleves){
-            echo "- $eleves\n";
+$creneaux = []; 
+while(true){
+    $debut = (int)readline("Heure d'ouverture : ");
+    $fin = (int)readline("Heure de femerture : ");
+    if($debut >= $fin){
+        echo "le crénaux ne peut etre enregistré car l'heure d'ouverture ($debut)  est supérieur à l'heure de femerture ($fin) ";
+    } else {
+        $creneaux[] = [$debut , $fin];
+        $action = readline("voulez-vous enregistrer un nouveau créneau (o/n) : ");
+        if($action == 'n'){
+            break;
         }
-        echo "\n";
     }
+}
+print_r($creneaux);
