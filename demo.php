bb03670556d7370c1ce6cd690e811563a9f57a86
eurin-lumen <eurindelagoldcoast@gmail.com>
2022-07-14 23:30:35 +0100

    la boucle ; for

--- demo.php
+++ demo.php
@@ -1,7 +1,12 @@
 <?php
 $chiffre = null; 
-while($chiffre !== 10 ){
-    $chiffre = (int) readline('Entrez une heure : ');
+// while($chiffre !== 10 ){
+//     $chiffre = (int) readline('Entrez une heure : ');
+//     break;
 
+// }
+// echo "Vous avez gagné";
+
+for($i = 0 ; $i < 10; $i++){
+    echo "- $i \n"; 
 }
-echo "Vous avez gagné";
