21ecb265b7c0b313c85fb54e2b82fbd3c3e4958c
eurin-lumen <eurindelagoldcoast@gmail.com>
2022-07-19 09:05:09 +0100

    Adulte -reserved

--- profil.php
+++ profil.php
@@ -1,25 +1,45 @@
-<?php
-$utilisateur = $_COOKIE['utilisateur'];
-var_dump(unserialize($utilisateur));
-require 'elements/header.php';
-?>
 <?php 
-if($nom) : ?>
-    <h1>Bonjour <?=htmlentities($nom)  ?> </h1>
-    <a href="profil.php?action=deconnecter">Se déconnecter</a>
+$age = null; 
+if(!empty($_POST['birthday'])){
+    setcookie('birthday', $_POST['birthday']);
+    $_COOKIE['birthday'] = $_POST['birthday'];
+}
+if(!empty($_COOKIE['birthday'])){
+        $birthday =(int) $_COOKIE['birthday'];
+        $age = date('Y') - $birthday;
+
+}
+
+
+
+require 'elements/header.php'; ?>
+
+<?php if($age && $age >= 18): ?>
+    <h1>Du contenu réservé aux adultes </h1>
+<?php elseif ($age !== null): ?>
+    <div class="alert alert-danger">Vous n'avez pas l'age pour accéder a ce contenue</div>
+
+<?php else : ?>
+
+<form action="" method="POST">
+    <div class="form-group">
+        <label for="birthday"> Section réservée pour les adultes, entre votre année de naissance, Entré votre année de naissance</label>
+        <select name="birthday" class = "form-control"id="birthday">
+            <?php for($i = 2010; $i > 1919; $i--):?>
+            <option value="<?= $i ?>"><?= $i ?></option>
+            <?php endfor  ?>
+        </select>
+    </div>
+    <button type="submit">Envoyez</button>
 <?php endif ?>
 
-<div class="container">
-    <br>
-    <form action="" method="POST"> 
+</form>
 
-        <div class="form-group">
-            <input class="form-control" type="text" value="" name="nom" placeholder="Votre nom"> <br>
-            <button class="btn btn-primary" type="submit">Se connecter</button>
-        </div>
 
-    </form>
-</div>
+
+
+
+
 <br>
 
 <?php
