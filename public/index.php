32e99a566bcde4fc547feeb16acc022511ec17ab
eurin-lumen <eurindelagoldcoast@gmail.com>
2022-07-20 01:25:27 +0100

    AltoRouter

--- public/index.php
+++ public/index.php
@@ -1,17 +1,24 @@
 <?php 
 require '../vendor/autoload.php';
+//require '../vendor/altorouter/AltoRouter.php';
 require '../elements/header.php';
 $uri = $_SERVER["REQUEST_URI"];
-if($uri === "/nous contacter"){
-    require '../template/contact.php';
-}elseif($uri === '/'){
-    require '../template/home.php';
-}else{
-    echo 404;
+$router = new AltoRouter();
+$router->map('GET','/' , function(){
+    echo "salut";
+});
+$router->map('GET','/nous-contacter' , function(){
+    echo "Nous contacter";
+});
+$router->map('GET','/blog/[*:slug]-[i:id]' , function($slug, $id ){
+    echo "Je suis sur l'article $slug avec le numero $id ";
+});
+
+$match = $router->match();
+if($match !== false){
+    require '../elements/header.php';
+    call_user_func_array($match['target'], $match['params']);
+    require '../elements/footer.php';
+
 }
-
-
-
-
-require '../elements/footer.php';
 
